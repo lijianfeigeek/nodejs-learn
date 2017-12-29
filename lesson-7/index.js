a8337f2a3ce344db1a61d67a34fd10a0ad409930
lijianfei <lijianfei@sishuxuefu.com>
2017-12-29 15:00:24 +0800

    lesson-7

---
+++ lesson-7/index.js
@@ -0,0 +1,45 @@
+/**
+ 模块化——
+1.系统模块：http、querystring、url
+2.自定义模块
+3.包管理器
+
+系统模块：
+
+我用人头担保，程序一定是这样的
+断言：
+
+www.baidu.com->
+getServers
+
+Crypto	加密
+Events	事件
+Net	    网络操作，邮件
+OS	    操作系统信息
+Path	处理文件路径，目录，扩展名
+REPL    nodejs命令行
+Stream	流操作,读一点发一点
+        readfile 全读
+Timers	定时器 Interval ,timeout
+TTY     链接远端命令行
+UDP
+URL
+Util    工具
+V8      
+VM      nodejs 运行在V8虚拟机上
+ZLIB	压缩,本质上是一种流
+  
+
+
+require
+
+-----------------------------------------------------------------------------
+
+/usr/tmp/aaa/a.txt
+/usr/tmp/aaa/
+a.txt
+.txt
+
+-----------------------------------------------------------------------------
+自定义模块
+ */ 
