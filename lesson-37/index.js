77ce6a573e20d79c21d7fc40be4c69e6b50a4ba2
lijianfei <lijianfei@sishuxuefu.com>
2018-01-05 11:29:37 +0800

    express 下一代 koa 框架

---
+++ lesson-37/index.js
@@ -0,0 +1,27 @@
+/*
+
+Promise——解决异步操作
+
+异步操作：性能高、写程序非常麻烦
+同步操作：性能低、写程序很简单
+
+？：性能高、很简单
+
+Promise、generator、async/await
+KOA
+
+NodeJS：
+1.数据
+2.模块化
+	npm
+	上传
+3.系统模块
+	http
+	fs
+	crypto
+4.数据库
+	4大语句
+
+KOA
+
+*/ 
